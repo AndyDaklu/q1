f69baa241591f21f5e99ef67454cd13e621f6983
AndyDaklu <andycerapalma28@gmail.com>
2026-08-27 09:01:51 +0800

    Update q1_sg5_a1_arayat_palma.py

--- q1_sg5_a1_arayat_palma.py
+++ q1_sg5_a1_arayat_palma.py
@@ -1,1 +1,14 @@
+# RPG HERO GAME  
+class Hero:
+  def __init__(self, name, hp):
+    self.name = name
+    self.hp = hp
+  def take_damage(self, amount):
+    self.hp -= amount
 
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+arthur.damage(10)
+print(f"{arthur.name}'s HP:{arthur.hp}")
+print(f"{morgana.name}'s HP:{morgana.hp}")
